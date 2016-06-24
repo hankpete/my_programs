f6dba395447e66d78da922c0adc157dde5a0f9c1
hankpete <hptrsn@comcast.net>
2016-06-24 14:40:44 -0700

    624

--- alpha.js
+++ alpha.js
@@ -1,32 +1,110 @@
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
-var names = ["Henry Peterson", "Carol Rosenfield", "Jim Sullivan"];
+////////////////////////////////////////////////////
+// alpha.js
+///////////////////////////////////////////////////
 
-//names = alphabetize(names);
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 names = ["amy kat", "berry jam", "dan ricky", "joe leaf", "joel kardi", "rik kart", "steve pete", "steve pratt", "zarg hat"];
-// names = put_in_alpha(names, "cara pete");
-// names = put_in_alpha(names, "joeseph tail");
-// names = put_in_alpha(names, "dan ricker");
-// names = put_in_alpha(names, "be me");
-// names = put_in_alpha(names, "amy katherine");
 jsonNames = jsonify(names);
 
-// d3.select("body").selectAll("p")
-//     .data(names)
-//   .enter()
-//     .append("p")
-//     .text(function(d) {return d;});
+alpha1 = indexIsGoesAfter(jsonNames);
+
+document.getElementById("names").innerHTML = alpha1;
 
 function jsonify(list) {
+    
     jsonList = [];
+    
     for (var i = 0; i < list.length; i++) {
         var spaceIndex = list[i].indexOf(" ");
         var first = list[i].slice(0, spaceIndex);
         var last = list[i].slice(spaceIndex, list[i].length);
         jsonList.push({ "first" :  first  , "last" : last });
     }
+  
     return jsonList;
 }
+
+
+function goesBefore(newJsonName, oldJsonName) {
+  
+  //returns true if new goes before old, false otherwise
+  
+  var newLast = newJsonName.last;
+  var newFirst = newJsonName.first;
+  var oldLast = oldJsonName.last;
+  var oldFirst = oldJsonName.first;
+  
+  for (var i = 0; i < Math.min(newLast.length, oldLast.lenght); i++) {
+    
+    if (newLast == oldLast) {
+      break;
+    }
+    
+    if (alphabet.indexOf(newLast[i]) > alphabet.indexOf(oldLast[i])) {
+      return false;
+    } else if (alphabet.indexOf(newLast[i]) < alphabet.indexOf(oldLast[i])) {
+      return true;
+    }
+   
+  }
+  
+  //same last names
+  for (var i = 0; i < Math.min(newFirst.length, oldFirst.lenght); i++) {
+    
+    if (newFirst == oldFirst) {
+      //same name, doesnt matter
+      return false;
+    }
+    
+    if (alphabet.indexOf(newLast[i]) > alphabet.indexOf(oldLast[i])) {
+      return false;
+    } else if (alphabet.indexOf(newLast[i]) < alphabet.indexOf(oldLast[i])) {
+      return true;
+    }
+    
+  }
+  
+}
+
+
+//alg 1
+function indexIsGoesAfter(jsonList) {
+  
+  //put each name at the index that is same num as how many names it is after
+  
+  var alphaJsonList = jsonList;
+  
+  for (var i = 0; i < jsonList.length; i++) {
+    var numGoesAfter = 0;
+    for (var j = 0; j < jsonList.length && j != i; i++) {
+      if (!goesBefore(jsonList[i], jsonList[j])) {
+        numGoesAfter += 1;
+      }      
+    }
+    alphaJsonList[numGoesAfter] = jsonList[i];
+  }
+  
+  return alphaJsonList;  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function numberize(letter) {
 //   for (var i = 0; i < alpha.length; i++) 
@@ -88,4 +166,3 @@ function jsonify(list) {
 //   }
 //   return final_alpha;
 // }
-
