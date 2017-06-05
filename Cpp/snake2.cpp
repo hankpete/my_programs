d09234190e8478400541c06ccb5d451a75a9ab84
Henry Peterson <hankpete97@gmail.com>
2017-06-05 14:16:47 -0700

    fancy snake

--- Cpp/snake2.cpp
+++ Cpp/snake2.cpp
@@ -1,30 +1,27 @@
 //The classic game of snake, terminal style
-#include <iostream>
-#include <string>
 #include <ncurses.h>
+#include <string>
+#include <unistd.h>  //sleep
+#include <time.h>
 using namespace std;
 
 class Board {
     private:
-        int n = 10;     //n x n board
+        static const int n = 20;     //n x n board
+                        //using 0 and n-1 as walls
         int tail = 0;   //length of tail
-        int mx = rand() % n;     //mouse x
-        int my = rand() % n;     //mouse y
-        char dir = 'w'; //direction of snake movement (WASD)
-        int sx[100] = {-1};    //snake segments coords
-        int sy[100] = {-1};
+        int mx = rand() % (n-2) + 1;     //mouse x
+        int my = rand() % (n-2) + 1;     //mouse y
+        char dir; //direction of snake movement
+        int sx[(n-2)*(n-2)] = {-1};    //snake segments coords
+        int sy[(n-2)*(n-2)] = {-1};
         int dt;  //speed
+        int start = 1;  //has the game just started
     public:
         int cont = 1;   //continue game
         Board() {
             //start curses mode
             initscr();
-            //colors on?
-            if (has_colors() == FALSE) {
-                endwin();
-                cout << "Your terminal does not support color\n";
-                cont = 0;
-            }
             //use colors
             start_color();
             init_pair(1, COLOR_GREEN, COLOR_BLACK); //snake
@@ -32,13 +29,21 @@ class Board {
             //input
             cbreak();   //no need to press enter
             noecho();   //don't show keys typed
+            keypad(stdscr, TRUE);  //special keys
             //start head of snake somewhere
-            sx[0] = rand() % n;
+            sx[0] = rand() % (n-2) + 1;
             if (sx[0] == mx) {
-                sy[0] = (my + 1) % n;
+                sy[0] = (my + 1) % (n-2) + 1;
             } else {
-                sy[0] = rand() % n;
+                sy[0] = rand() % (n-2) + 1;
             }
+            //make good dir
+            if (sx[0] > n / 2) {
+                dir = 'w';
+            } else {
+                dir = 'e';
+            }
+
             get_speed();
         }
         void get_speed() {
@@ -47,17 +52,17 @@ class Board {
             printw("\n    1 - Snail\n    2 - Worm\n    3 - Snake\n    4 - JEDI\n");
             refresh();
             char c = getch();
-            switch (c) {
-                case '1':
-                    dt = 1000;
-                case '2':
-                    dt = 500;
-                case '3':
-                    dt = 250;
-                case '4':
-                    dt = 75;
-                default:
-                    c = getch();
+            while (c != '1' && c != '2' && c != '3' && c != '4') {
+                c = getch();
+            }
+            if (c == '1') {
+                dt = 550;
+            } else if (c == '2') {
+                dt = 350;
+            } else if (c == '3') {
+                dt = 150;
+            } else {
+                dt = 50;
             }
         }
                 
@@ -73,50 +78,74 @@ class Board {
         void show() {
             clear();    //remove old board
             //loop through x and y. (0,0) is top left
-            string text = "    SCORE: " + to_string(tail) + "\n\n"; 
+            string text = "SCORE: " + to_string(tail) + "\n\n"; 
             printw( text.c_str() );
             for (int y = 0; y < n; y++) {
                 for (int x = 0; x < n; x++) {
                     if (x == mx && y == my) {
                         attron(COLOR_PAIR(2));
-                        printw("o");
+                        printw("x");
                         attroff(COLOR_PAIR(2));
                     } else if (is_snake(x,y)) {
                         attron(COLOR_PAIR(1));
                         if (x == sx[0] && y == sy[0]) {
-                            printw("X");
+                            printw("x");
                         } else{
                             printw("x");
                         }
                         attroff(COLOR_PAIR(1));
+                    } else if (x == 0 || x == n - 1 || y == 0 || y == n - 1) {
+                        printw("X");
                     } else {
-                        printw(".");
+                        printw(" ");
                     }
                     //add some space to make it a square board
                     printw(" ");
                 }
                 printw("\n");
+                curs_set(0); // hide cursor
             }
             //print it to the real screen
             refresh();
+            //slower beginning
+            if (start == 1) {
+                start = 0;
+                printw("\n3..");
+                refresh();
+                sleep(1);
+                printw("2..");
+                refresh();
+                sleep(1);
+                printw("1..");
+                refresh();
+                sleep(1);
+                printw("GO!");
+                refresh();
+            }
         }
         void ask_dir() {
             timeout(dt);  //it moves!
-            char new_dir = getch();
-            if (new_dir == 'w' || new_dir == 'a' || new_dir == 's' || new_dir == 'd') {
-                dir = new_dir;
+            int new_dir = getch();
+            if (new_dir == KEY_UP) {
+                dir = 'n';
+            } else if (new_dir == KEY_DOWN) {
+                dir = 's';
+            } else if (new_dir == KEY_LEFT) {
+                dir = 'w';
+            } else if (new_dir == KEY_RIGHT) {
+                dir = 'e';
             }
         }
         void move() {
             int dx,dy;
-            if (dir == 'w') {
+            if (dir == 'n') {
                 dx = 0; dy = -1;
-            } else if (dir == 'd') {
-                dx = 1; dy = 0;
+            } else if (dir == 'w') {
+                dx = -1; dy = 0;
             } else if (dir == 's') {
                 dx = 0; dy = 1;
-            } else {
-                dx = -1; dy = 0;
+            } else if (dir == 'e') {
+                dx = 1; dy = 0;
             }
             //change all but head of snake to one ahead
             for (int i = tail + 1; i > 0; i--) {
@@ -127,7 +156,7 @@ class Board {
             sx[0] += dx;
             sy[0] += dy;
             //end if on wall or self
-            if (sx[0] >= n || sx[0] < 0 || sy[0] >= n || sy[0] < 0) {
+            if (sx[0] >= n-1 || sx[0] <= 0 || sy[0] >= n-1 || sy[0] <= 0) {
                end_game();
             } 
             for (int i = 1; i < tail + 1; i++) {
@@ -141,14 +170,14 @@ class Board {
                 if (tail + 1 == n * n) {
                     win_game();
                 } else {
-                    int r = rand() % (n*n - tail - 1);  //a number between 0 and the spaces left
+                    int r = rand() % ((n-2)*(n-2) - tail - 1);  //a number between 0 and the spaces left
                     int count = 0;
-                    mx = -1 ; my = 0;
+                    mx = 0; my = 1;
                     while (count <= r) {
-                        if (mx < n - 1) {
+                        if (mx < n - 2) {
                             mx++;
                         } else {
-                            mx = 0;
+                            mx = 1;
                             my++;
                         }    
                         if (! is_snake(mx,my)) {
@@ -162,7 +191,7 @@ class Board {
             attron(COLOR_PAIR(2));
             printw("\nGAME OVER");
             attroff(COLOR_PAIR(2));
-            printw("\n\n(q to quit)");
+            printw("\n\n('q' to quit)");
             refresh();
             timeout(-1);  //indef
             char c = getch();
@@ -177,7 +206,7 @@ class Board {
             attron(COLOR_PAIR(1));
             printw("\nYOU WIN");
             attroff(COLOR_PAIR(1));
-            printw("\n\n(q to quit)");
+            printw("\n\n('q' to quit)");
             refresh();
             timeout(-1); //indef
             char c = getch();
@@ -191,7 +220,7 @@ class Board {
 };
 
 int main() {
-    
+    srand( time(NULL) );
     
     Board brd;
     while (brd.cont == 1) {
